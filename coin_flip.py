52f61b1d39461e3d43d03cd9d9626629b6b2a98a
Cody <hello@codycollins.me>
2020-05-12 21:49:12 -0400

    Build of GUI layout sans functionality

--- coin_flip.py
+++ coin_flip.py
@@ -1,48 +1,36 @@
 #This is a game of Flip the Coin. The first to 5 wins.
+from tkinter import *
+from tkinter import ttk
 import random
+import time
+root = Tk()
+root.title('Coin Flip')
+mainframe = ttk.Frame(root, padding='3 3 12 12')
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+ttk.Label(mainframe, text='FLIP THE COIN').grid(column=2, row=1)
 computer_score = 0
 player_score = 0
-print("FLIP THE COIN\nHello, I'm the Computer you'll be playing against.\nThe first to 5 points wins.")
-while (computer_score < 5):
-    while (player_score < 5):
-        print('Choose: Heads or Tails')
-        player_coin = input()
-        print('You chose: ' + player_coin)
-        print('Flipping the coin. . .')
-        coin_face = random.randint(0, 1)
-        if(coin_face == 0):
-            coin_face = 'Heads'
-        else:
-            coin_face = 'Tails'
-        print("It's " + coin_face + "!")
-        if(coin_face == player_coin):
-            player_score = player_score + 1
-        else:
-            computer_score = computer_score + 1
-        print(f'You have a score of {player_score}. The Computer has a score of {computer_score}.')
-        if(player_score == 5):
-            print('You win! Congrats!')
-            print('Would you like to play again? Y or N?')
-            play_again = input()
-            if(play_again == 'Y'):
-                print('Reloading. . .')
-                computer_score = 0
-                player_score = 0
-                continue
-            else:
-                print('Thanks for playing.')
-                break
-        elif(computer_score == 5):
-            print('You lose. Sorry!')
-            print('Would you like to play again? Y or N?')
-            play_again = input()
-            if(play_again == 'Y'):
-                print('Reloading. . .')
-                computer_score = 0
-                player_score = 0
-                continue
-            else:
-                print('Thanks for playing.')
-                break
-    break
-print('Goodbye!')
+ttk.Label(mainframe, text='Player').grid(column=1, row=4)
+ttk.Label(mainframe, text='Computer').grid(column=3, row=4)
+ttk.Label(mainframe, text=player_score).grid(column=1, row=5)
+ttk.Label(mainframe, text=computer_score).grid(column=3, row=5)
+ttk.Label(mainframe, text="5 points wins.\nChoose: Heads or Tails").grid(column=2, row=2)
+def flip_coin():
+    values = ['heads', 'tails']
+    return random.choice(values)
+def get_player_choice():
+    return player_choice
+def choose_heads(player_choice = 'heads'):
+    return
+def choose_tails(player_choice = 'tails'):
+    return
+def point_player():
+    pass
+def point_computer():
+    pass
+ttk.Button(mainframe, text='Heads', command=choose_heads).grid(column=1, row=3)
+ttk.Button(mainframe, text='Tails', command=choose_tails).grid(column=3, row=3)
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+root.mainloop()
